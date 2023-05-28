6e3af0021d1d0de4881111225a6777dbb0d6a198
soscooter <lelya.rumynin@gmail.com>
2023-05-28 13:30:39 +0300

    задание добвалено

---
+++ 3 laba/cmake-build-debug/CMakeFiles/3_laba.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "3_laba"
+  "3_laba.pdb"
+  "CMakeFiles/3_laba.dir/main.cpp.o"
+  "CMakeFiles/3_laba.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3_laba.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
